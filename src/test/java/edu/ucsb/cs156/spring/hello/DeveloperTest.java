25333a6ea5b4b92bfbcba6d12df17952d0c8c62e
Amy <amy_li@ucsb.edu>
2025-10-07 17:59:18 -0700

    updated with gitid

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,6 +25,11 @@ public class DeveloperTest {
         assertEquals("Amy L.", Developer.getName());
     }
 
+    @Test
+    public void getGithubId_returns_correct_githubId(){
+        assertEquals("amyli2027", Developer.getGithubId());
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
